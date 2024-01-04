8435ef1d9f1cccd4feb9c388d32b903ddcdbd2d7
DaniOkuma <daniellemokuma@gmail.com>
2024-01-04 13:08:06 -0300

    crud produto

--- farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
+++ farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,15 +1,21 @@
 package com.generation.farmacia.model;
 
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
 
 @Entity
 @Table(name = "tb_categorias")
@@ -29,6 +35,10 @@ public class Categoria {
 	@Size(min = 3, max = 50, message = "O Atributo tipo deve conter no mínimo 3 e no máximo 50 caracteres.")
 	private String tipo;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produtos;
+	
 	public Long getId() {
 		return id;
 	}
@@ -52,7 +62,13 @@ public class Categoria {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 	
-
-
 }
